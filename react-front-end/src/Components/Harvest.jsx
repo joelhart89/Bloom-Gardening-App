61a01a575f3fe9a92a48e89d1a1f2ee9bc22eb9c
Wagsnerlund <chelseawagner17@gmail.com>
2021-05-16 20:21:02 -0700

    added progress bar

--- react-front-end/src/Components/Harvest.jsx
+++ react-front-end/src/Components/Harvest.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import Button from '@material-ui/core/Button';
 import useAppData from "../hooks/useAppData";
 
@@ -15,9 +16,6 @@ import {
 } from "react-router-dom";
 const moment = require('moment');
 const axios = require('axios');
-
-
-
 
 const useStyles = makeStyles({
   root: {
@@ -46,23 +44,41 @@ const useStyles = makeStyles({
   twidth: {
     flex: 1,
     width: '100%'
+  },
+  progress: {
+    height: '20px',
+    width: "100%",
+    backgroundColor: 'lightGrey',
+  },
+  progressBar: {
+    height: '20px',
+    width: '40%',
+    backgroundColor: 'green',
   }
 });
 
-
-
-
 // test to map over all planted veg and calculate harvest dates
 
-
-  
-
-
+const getProgress = function (dtePlanted, daysToHarvest) {
+  let planted = moment(dtePlanted);
+  let currentDay = moment();
+  let days = planted.diff(currentDay, "days") * 100 / daysToHarvest;
+  console.log("planted", planted);
+  console.log("current", currentDay);
+  console.log("harvest", daysToHarvest);
+  console.log("days:", days);
+  if (days < 0) {
+    return "100%";
+  } else {
+    return Math.round(days) + "%";
+  }
+}
 
 export default function Harvest() {
   const classes = useStyles();
   let { id } = useParams();
   const { state, setState, markComplete } = useAppData();
+  console.log(state.harvest);
 
 
   useEffect(() => {
@@ -71,50 +87,51 @@ export default function Harvest() {
   const removeHarvest = function (name) {
     const found = state.harvest.find(harvest => harvest.name === name);
     const newHarvest = state.harvest.filter(harvest => harvest !== found);
-    setState({...state, harvest: newHarvest})
+    setState({ ...state, harvest: newHarvest })
   }
 
   // getHarvestDate()
 
-  const harvest_date = function (planted, harvest) {
-  const harvest_date = moment(planted).add(harvest, 'days')
-  const counter = moment(harvest_date).fromNow();
-  return counter;
+  const harvestDate = function (planted, harvest) {
+    const harvest_date = moment(planted).add(harvest, 'days')
+    const counter = moment(harvest_date).fromNow();
+    console.log(harvest);
+    return counter;
   }
+  
 
 
   return (
     <Card className={classes.root}>
       <CardContent className={classes.twidth}>
-      <h2>Harvesting Schedule</h2>
+        <h2>Harvesting Schedule</h2>
+
         <table className={classes.twidth}>
           <thead >
             <tr >
-              <th>Task</th>
-              <th>Date</th>
-              <th>Complete</th>
+              <th></th>
+              <th>Progress</th>
             </tr>
           </thead>
           <tbody >
-          {state.harvest.map(x => 
-          <tr>
-          <td>
-              {x.name}
-            </td>
-            <td>
-              {harvest_date(x.planted_date, x.harvest_date)}
-            </td>
-            <td>
-            <CardActions>
-              <Button size="small" variant="contained" 
-              onClick={() => removeHarvest(x.name)}
-              color="primary">Complete</Button>
-            </CardActions>
-            </td>
-          </tr>
+            {state.harvest.map(x =>
+              <tr>
+                <td >
+                  <img
+                    className="avatar"
+                    src={x.avatar_url}
+                    alt="img"
+                  />
+                </td>
+                <td>
+                  <div className={classes.progress} >
+                    <div className={classes.progressBar} style={{ width: getProgress(x.planted_date, x.harvest_date) }}></div>
+                  </div>
+                </td>
+              </tr>
             )}
           </tbody>
-          
+
         </table>
       </CardContent>
     </Card>
