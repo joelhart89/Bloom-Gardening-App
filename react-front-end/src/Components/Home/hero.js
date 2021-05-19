85c09ec793d903f43d34ee57d1a186246171ee23
Joel <joel.d.hart1@gmail.com>
2021-05-19 18:21:08 +0000

    fixed get started button on home

--- react-front-end/src/Components/Home/hero.js
+++ react-front-end/src/Components/Home/hero.js
@@ -5,28 +5,28 @@ const places = [
     description:
       "Starting your first garden can be overwhelming... What plants should I choose? When should I water? Bloom answers these questions for you, providing you with the knowledge and tools to know what plants to choose and how to care for them. ",
     imageUrl: process.env.PUBLIC_URL + '/assets/userStories.jpg',
-    time: 1500,
+    time: 1000,
   },
   {
     title: 'Planting Instructions',
     description:
       "We will help you get started by providing you with the knowledge so you know how to plant each vegetabale or fruit. With weather notifications and tips on how much sun your produce will need. You will be well on your way to having fresh produce every year.",
     imageUrl: process.env.PUBLIC_URL + 'assets/plantingInstructions.jpg',
-    time: 1500,
+    time: 2000,
   },
   {
     title: "We'll Remind You",
     description:
-      "Forgetting when you need to water and care for your produce can kill your garden. Bloom helps us absent minded folks by sending you notifications so you know when to water, fertilize, and weed. We will even give you weather updates to let you know you can take a day off.",
+      "Forgetting how and when to care for your produce can kill your garden. Bloom helps us absent minded folks by sending notifications so you know when to water, fertilize, and weed. We will even give you weather updates to let you know you can take a day off.",
     imageUrl: process.env.PUBLIC_URL + 'assets/watering.jpg',
-    time: 1500,
+    time: 3000,
   },
   {
-    title: "Interested?",
+    title: "Get Started?",
     description:
       "Can't wait? Click the link below to start learning about fruits and vegetables. We will send you to our planning page so you can pick produce that fits your needs.Click build your garden and we will provide your own personal garden plan. ",
     imageUrl: process.env.PUBLIC_URL + 'assets/getStarted1.jpg',
-    time: 1500,
+    time: 4000,
   },
 ];
 
