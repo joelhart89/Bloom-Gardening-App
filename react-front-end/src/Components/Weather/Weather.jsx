3f093f7beb6163b371268e5cb1f583ed4cf5eabe
Wagsnerlund <chelseawagner17@gmail.com>
2021-05-19 13:40:06 -0700

    updated styling to dashboard components

--- react-front-end/src/Components/Weather/Weather.jsx
+++ react-front-end/src/Components/Weather/Weather.jsx
@@ -28,14 +28,14 @@ export default function Weather() {
     let frost = 0;
     let heat = 0;
     let rain = 0;
-  
+
     for (const day of weatherData) {
       if (day.temp.min <= 273.15) frost++;
       if (day.temp.max >= 305.37) heat++;
       if (day.rain) rain = rain + day.rain;
       // console.log(day.rain);
     }
-    
+
     let alert1;
     let alert2;
     let alert3;
@@ -46,33 +46,31 @@ export default function Weather() {
     if (heat > 0) {
       alert2 = <div className="alert-heat"><span>warning: </span>High Heat Warning. Early morning watering recommended.</div>;
     }
-    if (rain > 13) {
+    if (rain > 0) {
       alert3 = <div className="alert-rain"><span>warning: </span>More than 13mm of rain expected.</div>;
     }
     // console.log("rain:", rain);
     return (<div>{alert1}{alert2}{alert3}</div>);
   }
-  
+
   return (
-    <main>
-      <Card className="weather-card">
-        <section>
-          <h2>Vancouver, CA</h2>
-          <div>{getAlerts()}</div>
-          <div className="row">
-            {weatherData.map(w => (
-              <WeatherItem classname="centerCard"
-                key={w.weather[0].id}
-                description={w.weather[0].description}
-                icon={w.weather[0].icon}
-                tempMin={w.temp.min}
-                tempMax={w.temp.max}
-                dte={w.dt}
-              />
-            ))}
-          </div>
-        </section>
-      </Card>
+    <main className="weather-card">
+      <div className="weather-container">
+        <div className="weather-hdr">Vancouver, CA</div>
+        <div>{getAlerts()}</div>
+        <div className="row">
+          {weatherData.map(w => (
+            <WeatherItem classname="centerCard"
+              key={w.weather[0].id}
+              description={w.weather[0].description}
+              icon={w.weather[0].icon}
+              tempMin={w.temp.min}
+              tempMax={w.temp.max}
+              dte={w.dt}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
