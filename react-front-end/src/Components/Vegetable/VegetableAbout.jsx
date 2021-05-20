a3a700e1d61fbd3336e53e7560e170f64872a28f
Cole Kubossek <cole.kubossek@gmail.com>
2021-05-20 03:25:50 +0000

    adds info tab on top of vegetables page

--- react-front-end/src/Components/Vegetable/VegetableAbout.jsx
+++ react-front-end/src/Components/Vegetable/VegetableAbout.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import './VegetableAbout.scss';
 import useAppData from "../../hooks/useAppData";
+import Alert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  alert: {
+    width: '90%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+      fontSize: "20px",
+    },
+    marginBottom: '40px',
+  },
+}));
 
 export default function VegetableAbout() {
   const { state } = useAppData();
+  const [info, setInfo] = React.useState(true);
+  const classes = useStyles();
+
+
 
   return (
     <div className="content">
+       <div className={classes.alert}>
+      <Collapse in={info}>
+        <Alert
+          severity="info"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setInfo(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          <div className="fontSize">
+            Below you will find a short discription about each vegetable. Use this resource to browse
+            and find what vegetables you want to grow. Note some of the health benefits of each one!
+            <br/>
+            <br/>
+            Once you have the information you need, click Build My Guarden to get started.
+          </div>
+        </Alert>
+      </Collapse>
+    </div>
       {state.vegetables.map(veg => (
         <div className="mainCard">
           <div className="flipCard">
--- react-front-end/src/Components/Vegetables.jsx
+++ react-front-end/src/Components/Vegetables.jsx
@@ -6,7 +6,6 @@ import VegetableDrawer from './VegetableDrawer'
 import Alert from '@material-ui/lab/Alert';
 import IconButton from '@material-ui/core/IconButton';
 import Collapse from '@material-ui/core/Collapse';
-import Button from '@material-ui/core/Button';
 import CloseIcon from '@material-ui/icons/Close';
 import './Vegetables.scss';
 
