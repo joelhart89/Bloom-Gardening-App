d4fd197f18aed834cbb3c44000f9410a33ed7c47
Cole Kubossek <cole.kubossek@gmail.com>
2021-05-20 04:59:01 +0000

    sort functionality done

--- react-front-end/src/Components/Vegetables.jsx
+++ react-front-end/src/Components/Vegetables.jsx
@@ -7,6 +7,8 @@ import Alert from '@material-ui/lab/Alert';
 import IconButton from '@material-ui/core/IconButton';
 import Collapse from '@material-ui/core/Collapse';
 import CloseIcon from '@material-ui/icons/Close';
+import Button from "@material-ui/core/Button";
+import useAppData from "../hooks/useAppData";
 import './Vegetables.scss';
 
 
@@ -18,12 +20,34 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "20px",
     },
   },
+  sortButton: {
+    display: "flex",
+    justifyContent: "center",
+    marginLeft: '22%',
+    marginBottom: -100,
+    borderLeft: 150,
+    borderRight: 150,
+    width: '175px',
+  },
+  test: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: '2%',
+  },
+  button: {
+    width: '100px',
+    margin: '15px',
+    marginLeft: '15%'
+  }
 }));
 
 
 export default function Vegetables() {
   const [open, setOpen] = useState(false);
   const [info, setInfo] = React.useState(true);
+  const [veg, setVeg] = useState([]);
+  const { state, setState } = useAppData();
 
   const classes = useStyles();
 
@@ -34,32 +58,42 @@ export default function Vegetables() {
     setOpen(false);
   };
 
-  const[veg, setVeg] = useState([]);
-   
-  useEffect(() => {
-    getAllVeg();
-  }, []);
-
-  const renderVegetableCard = (veg) =>{
-    const data = veg.map(element => {
-      return (
-        <VegetableCard
-         {...element}
-        onClick ={handleDrawerOpen} />
-      )
-    }) 
-    return data
+  const easiest = () => {
+    let difficulty = state.vegetables.sort((a, b) => (a.difficulty > b.difficulty) ? 1 : -1)
+    console.log('difficulty', difficulty)
+    setState({...state, vegetables: difficulty})
   }
 
-  const getAllVeg = () => {
-    axios.get ('/api/vegetables')
-    .then ((res) =>{
-      const allVeg = res.data;
-      //add data to state
-      setVeg(allVeg)
-    })
-    .catch(error => console.error(`Error: ${error}`))
+  const sun = () => {
+    let sunnest = state.vegetables.sort((a, b) => (a.sun_required > b.sun_required) ? 1 : -1)
+    setState({...state, vegetables: sunnest})
   }
+
+  const water = () => {
+    let wateriest = state.vegetables.sort((a, b) => (a.water_time > b.water_time) ? 1 : -1)
+    setState({...state, vegetables: wateriest})
+  }
+
+  // const renderVegetableCard = (veg) =>{
+  //   const data = veg.map(element => {
+  //     return (
+  //       <VegetableCard
+  //        {...element}
+  //       onClick ={handleDrawerOpen} />
+  //     )
+  //   }) 
+  //   return data
+  // }
+
+  // const getAllVeg = () => {
+  //   axios.get ('/api/vegetables')
+  //   .then ((res) =>{
+  //     const allVeg = res.data;
+  //     //add data to state
+  //     setVeg(allVeg)
+  //   })
+  //   .catch(error => console.error(`Error: ${error}`))
+  // }
 
   return (
     <div className="box"> 
@@ -84,12 +118,36 @@ export default function Vegetables() {
             Read through the vegetables and add them your basket.
             When you're ready, click Build My Garden to get a personlized gardening plan.
             <br/>
+            <br/>
             Peppers indicate the growing difficulty.
+            <br/>
+            <br/>
+            You can sort by: The easiest vegetables to grow, the required amount of sun, and watering frequency. 
           </div>
         </Alert>
       </Collapse>
-    </div>
-        {renderVegetableCard(veg)}
+      <div className={"box2"}>
+        Sort by: 
+        <div className="spacing">
+        <Button className={classes.button} onClick={easiest} variant="contained" color="primary" >
+          Easiest
+        </Button >
+        <Button className={classes.button} onClick={sun} variant="contained" color="primary" >
+        Sun 
+        </Button >
+        <Button className={classes.button} onClick={water} variant="contained" color="primary" >
+        Water 
+        </Button >
+        </div>
+      </div>
+    </div >
+        {state.vegetables.map(element => {
+      return (
+        <VegetableCard
+         {...element}
+        onClick ={handleDrawerOpen} />
+      )
+    })}
       <VegetableDrawer 
       open = {open}
       handleDrawerOpen = {handleDrawerOpen}
